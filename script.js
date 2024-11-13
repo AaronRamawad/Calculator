11ac452689f9615419cc83c23e75b48c7902677f
TheSlumberingEgg <aramawad05@gmail.com>
2024-11-12 23:58:06 -0500

    Fix operate function and clean HTML

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if (b === 0) {
+        return "Nice Try Bro";
+    } else {
+        return a / b;
+    }
+    
 }
 
 let valueOne;
@@ -19,27 +24,58 @@ let operator;
 let valueTwo;
 
 function operate(valueOne, operator, valueTwo) {
-    switch(operator){
-        case "+":
-            add(valueOne, valueTwo);
-            break;
-        case "-":
-            subtract(valueOne, valueTwo);
-            break;
-        case "*":
-            multiply(valueOne, valueTwo);
-            break;
-        case "/":
-            divide(valueOne, valueTwo);
-            break;
+    if (operator === "+") {
+        return add(valueOne, valueTwo);
+    } else if (operator === "-") {
+        return subtract(valueOne, valueTwo);
+    } else if (operator === "*") {
+        return multiply(valueOne, valueTwo);
+    } else if (operator === "/") {
+        return divide(valueOne, valueTwo);
     }
-}
+};
 
 const display = document.querySelector('.display');
-const allButtons = document.querySelectorAll('button');
+const allNumberButtons = document.querySelectorAll('.number');
+const allOperatorButtons = document.querySelectorAll('.operator');
+let expression = "";
 
-allButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        display.textContent += button.textContent;
-    });
+allNumberButtons.forEach((button) => {
+    if (button.textContent !== "=") {
+        button.addEventListener("click", () => {
+            display.textContent += button.textContent;
+            expression = display.textContent;
+        });
+    }
+});
+
+allOperatorButtons.forEach((button) => {
+    if (button.textContent !== "AC") {
+        button.addEventListener("click", () => {
+            display.textContent += ` ${button.textContent} `;
+            expression = display.textContent;
+        });
+    }
+});
+
+const clearButton = allOperatorButtons[0];
+
+clearButton.addEventListener("click", () => {
+    display.textContent = "";
+    expression = "";
+});
+
+const equalButton = document.querySelector(".equal");
+
+equalButton.addEventListener("click", () => {
+    
+    let expressionArray = expression.split(" ");
+    let endValue = +expressionArray[0];
+    console.log({expressionArray});
+    for (let i = 1; i < expressionArray.length; i += 2) {
+        let operator = expressionArray[i];
+        let secondValue = +expressionArray[i + 1];
+        endValue = operate(endValue, operator, secondValue);
+    }
+    display.textContent = endValue;
 });
